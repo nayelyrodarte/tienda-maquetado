bf40e07abba953111b9c399a8a1c28948522ad4b
Nayely Rodarte <nayelyrodarte@gmail.com>
2020-11-09 14:02:24 -0600

    Update header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,52 @@
 import React from 'react';
 import SearchBar from '../components/SearchBar';
 import Shortcuts from '../components/Shortcuts';
-import { makeStyles } from '@material-ui/styles';
+import Logo from '../img/chaman-morado.png';
+import searchIcon from '../img/search-24px.png';
+import menuIcon from '../img/icons8-menu.svg';
 
-const logoStyles = makeStyles({
-  root: {},
-});
+import styled from 'styled-components';
+
+const HeaderWrapper = styled.div`
+  .mobileMenu {
+    img:nth-of-type(1),
+    img:nth-of-type(3) {
+      display: none;
+    }
+
+    @media (max-width: 768px) {
+      width: 100vw;
+      background-color: white;
+      display: grid;
+      grid-template-columns: repeat(3, 40%);
+      z-index: 10;
+      position: fixed;
+      box-shadow: -2px 4px 15px -4px #000000;
+
+      img:nth-of-type(1),
+      img:nth-of-type(3) {
+        display: block;
+      }
+
+      img {
+        margin: 0.5em 1em;
+        align-self: center;
+      }
+    }
+  }
+`;
 
 function Header() {
-  const logo = logoStyles();
   return (
-    <div className={logo.root}>
-      <img src='chaman-morado.png' alt='chaman-morado-logo' />
-      <SearchBar />
+    <HeaderWrapper>
+      <div className='mobileMenu'>
+        <img src={menuIcon} alt='menu' />
+        <img src={Logo} alt='logo' />
+        <img src={searchIcon} alt='search' />
+      </div>
       <Shortcuts />
-    </div>
+      <SearchBar />
+    </HeaderWrapper>
   );
 }
 
