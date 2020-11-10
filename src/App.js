32b5b5b62d7a13997067920cb92c58ae8e83a920
Nayely Rodarte <nayelyrodarte@gmail.com>
2020-11-09 22:05:32 -0600

    Update app.js imports

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import MenuBar from './components/MenuBar';
 import Carousel from './components/Carousel';
 import FeaturedProducts from './components/FeaturedProducts';
 import MidBanner from './components/MidBanner';
-import BrandsBanner from './components/BrandsBanner';
 import Footer from './components/Footer';
 
 import '../src/App.css';
